0316f453c4f2a06a56397b5b8f77515b93f1df35
Nozzin <gknozzin@gmail.com>
2016-12-06 18:13:43 -0500

    Update Age 100.py

--- Age 100.py
+++ Age 100.py
@@ -1,12 +1,15 @@
 def main():
     age = int(input('What is your age? '))
     name = input('What is your name? ')
+    
     year = 2016
+    
     while age < 100:
         age +=1
         year +=1
     
     howMany = int(input('How many times do you want the message? '))
+    
     for i in range(howMany):
         print ('Hello',name,'you will be 100 in ',year,'.')
         print('')
